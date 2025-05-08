ad836718732d7cf3405a52c9cd5c21ea716a5abb
liushaohuai5 <liush20@mails.tsinghua.edu.cn>
2025-05-08 00:56:14 -0500

    first commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="RL4Grid",
-    version="0.2",
+    version="0.1",
     packages=find_packages(),
     install_requires=[
         "gym==0.17.1",
